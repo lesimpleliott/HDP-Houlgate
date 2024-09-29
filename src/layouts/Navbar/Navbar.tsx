fbd2e98491544ee2da31506d8acce222c0e8fe40
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-29 19:45:12 +0200

    Logo // Create app logo component

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import LogoHDP from "../../components/LogoHDP";
 import useStoreMenu from "../../stores/menu.store";
 import BurgerButton from "./BurgerButton";
 import MenuLinks from "./MenuLinks";
@@ -17,10 +18,7 @@ const Navbar = () => {
         {/* LOGO + BURGER BUTTON */}
         <section className="flex items-center justify-between">
           <NavLink to="/" className="w-12" onClick={() => setMenuIsOpen(false)}>
-            <img
-              src="./hdpHoulgate_logo.svg"
-              alt="Hôtel de la plage Houlgate"
-            />
+            <LogoHDP />
           </NavLink>
           <BurgerButton className="md:hidden" />
         </section>
@@ -30,7 +28,7 @@ const Navbar = () => {
           className={`flex flex-1 flex-col gap-2 md:flex-row md:items-center md:justify-end md:gap-4`}
         >
           <MenuLinks className="flex flex-col md:flex-row md:gap-4" />
-          <MenuSettings className="flex justify-center gap-10 md:gap-2 py-4 md:py-0" />
+          <MenuSettings className="flex justify-center gap-10 py-4 md:gap-2 md:py-0" />
         </section>
       </div>
     </nav>
