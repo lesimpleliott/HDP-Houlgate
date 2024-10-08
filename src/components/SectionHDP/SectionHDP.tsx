cc927e460e840875f8104d8e2793a81963ce12f9
Eliott Lesimple <lesimpleliott@gmail.com>
2024-10-08 19:10:28 +0200

    SectionHDP // Added JSDoc comments and updated component description

--- src/components/SectionHDP/SectionHDP.tsx
+++ src/components/SectionHDP/SectionHDP.tsx
@@ -1,3 +1,15 @@
+/**
+ * Ce composant correspondant à une section de l'app HDP Houlgate.
+ * Elle peut etre utilisée pour afficher des informations ou des composants.
+ * La couleur de fond peut être inversée en utilisant la propriété `reversedColor`.
+ *
+ * @param {string} className - Classes CSS supplémentaires pour le conteneur de la section.
+ * @param {React.ReactNode} children - Contenu à afficher dans la section.
+ * @param {boolean} [reversedColor=false] - Indique si la couleur de fond doit être inversée. Par défaut, cette valeur est `false`.
+ *
+ * @returns {JSX.Element} Le composant SectionHDP.
+ */
+
 import ReversedColorContext from "./ReversedColorContext"; // Importe le contexte
 
 type SectionHDPProps = {
