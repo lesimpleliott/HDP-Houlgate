66ca11940acd902189be39987c1296b7107d65dd
Eliott Lesimple <lesimpleliott@gmail.com>
2024-10-17 22:25:01 +0200

    CollapsedSection // Create component and integrate it into Reviews and RoomsOverview

--- src/layouts/Reviews/ReviewsCards.tsx
+++ src/layouts/Reviews/ReviewsCards.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import dataReviews from "../../assets/data/reviews.json";
 
-const ReviewsCards = () => {
+const ReviewsCards = ({ className }: { className?: string }) => {
   const { i18n } = useTranslation();
 
   const formattedRating = (rating: number) => {
@@ -21,22 +21,26 @@ const ReviewsCards = () => {
   };
 
   return (
-    <div className="columns-1 gap-4 space-y-4 xsm:columns-2 md:columns-3 lg:columns-4">
+    <div
+      className={`columns-1 space-y-4 ${className} xsm:columns-2 md:columns-3 lg:columns-4`}
+    >
       {dataReviews.map((review, index) => {
         return (
           <article key={index} className="card relative break-inside-avoid p-4">
             {/* TEXTE */}
             <header className="w-full">
-              <h3 className="overflow-hidden text-ellipsis text-nowrap text-xl font-medium">
-                {review.authorAttribution.displayName}
-              </h3>
+              <div className="flex justify-between">
+                <h3 className="overflow-hidden text-ellipsis text-nowrap text-xl font-medium">
+                  {review.authorAttribution.displayName}
+                </h3>
+                <span className="ml-4 text-xl font-medium text-primary-500">
+                  {formattedRating(review.rating)}
+                  <span className="text-xs font-light">/5</span>
+                </span>
+              </div>
               <div className="flex items-center justify-between text-sm italic text-gray-400">
                 <p>{dateFormated(review.publishTime)}</p>
-                <div className="flex items-center gap-0.5 text-primary-500">
-                  <span className="mr-1 text-lg">
-                    {formattedRating(review.rating)}
-                    <span className="text-xs font-light">/5</span>
-                  </span>
+                <div className="flex items-center gap-0.5">
                   {[...Array(5)].map((_, starIndex) => (
                     <i
                       key={starIndex}
--- src/layouts/Rooms/RoomsOverview.tsx
+++ src/layouts/Rooms/RoomsOverview.tsx
@@ -1,81 +1,68 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import roomsData from "../../assets/data/rooms.json";
+import CollapsedSection from "../../components/CollapsedSection";
 import CtaButton from "../../components/CtaButton";
-import SectionHDP from "../../components/SectionHDP/SectionHDP";
 import SectionTitle from "../../components/SectionHDP/SectionTitle";
-import useTailwindBreakpoint from "../../hooks/useTailwindBreakpoint";
+import useTailwindBreakpoints from "../../hooks/useTailwindBreakpoint";
 import { RoomTypes } from "../../types/RoomTypes";
 import RoomHeader from "./RoomHeader";
 
 const RoomsOverview = ({ className }: { className?: string }) => {
+  const typedRoomsData: RoomTypes[] = roomsData as RoomTypes[];
   const { t } = useTranslation();
+  const { sm, md, lg } = useTailwindBreakpoints();
 
-  // Etat permettant de savoir si on affiche tous les chambres ou non
-  const [showAllRooms, setShowAllRooms] = useState(false);
-  // On récupère la taille de l'écran
-  const sm = useTailwindBreakpoint("sm");
-  const lg = useTailwindBreakpoint("lg");
-  // Détermine le nombre de chambres à afficher en fonction de la taille de l'écran
-  const roomsToShow: RoomTypes[] = (
-    showAllRooms
-      ? roomsData
-      : lg
-        ? // écran supérieur à lg (1024px) : on affiche 3 cards
-          roomsData.slice(0, 3)
-        : sm
-          ? // écran supérieur à sm (640px) : on affiche 4 cards
-            roomsData.slice(0, 4)
-          : // écran inférieur à sm (640px) : on affiche 3 cards
-            roomsData.slice(0, 3)
-  ) as RoomTypes[];
+  // Définir la variable en fonction des breakpoints
+  const displayRows = lg ? 1 : md ? 2 : sm ? 2 : 3;
+  // Taille des cartes
+  const cardHeight = 370; // Hauteur des cartes (en px)
+  const gap = 16; // Espace entre les cartes (gap-4 = 1rem = 16px)
+  const lineHeight = displayRows * (cardHeight + gap) + 40; // 40px pour le bouton 'voir plus'
 
   return (
-    <SectionHDP className={`${className} flex flex-col gap-4`}>
-      {/* TITRE + CTA */}
-      <section className="flex justify-between">
+    <section className={`w-full space-y-4 py-6 ${className}`}>
+      {/* INTRODUCTION */}
+      <section className="max-w-custom flex justify-between">
         <SectionTitle title={t("rooms.title")} />
         <CtaButton type="Navlink" to="/rooms">
           {t("common.seeAll")}
         </CtaButton>
       </section>
 
-      {/* CARDS */}
-      <section className="grid grid-rows-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {roomsToShow.map((room, index) => (
-          <article key={index} className="card flex flex-col p-3.5 ">
-            {/* IMAGE */}
-            <img
-              src={room.images[0]}
-              className="max-h-52 w-full rounded-t-lg object-cover"
-            />
-
-            {/* TITLE + INFOS */}
-            <RoomHeader room={room} />
-
-            {/* <!-- BOUTON --> */}
-            <NavLink
-              to={`/rooms#${room.id}`}
-              className="mt-2.5 rounded-md bg-gray-800 p-2 text-center text-sm font-semibold text-white hover:bg-gray-900"
+      {/* CHAMBRES */}
+      <CollapsedSection minHeight={`${lineHeight}px`} button="flat">
+        <section className="grid grid-rows-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {typedRoomsData.map((room, index) => (
+            <article
+              key={index}
+              className="card flex flex-col p-3.5"
+              style={{ height: `${cardHeight}px` }}
             >
-              {t("common.moreInfo")}
-            </NavLink>
-          </article>
-        ))}
-      </section>
+              {/* IMAGE */}
+              <img
+                src={room.images[0]}
+                className="h-52 w-full rounded-t-lg object-cover"
+              />
 
-      {/* BOUTON 'VOIR PLUS' */}
-      <button
-        onClick={() => setShowAllRooms(!showAllRooms)}
-        className="italic text-gray-400"
-      >
-        {showAllRooms ? t("common.showLess") : t("common.showMore")}
-        <i
-          className={`fa-solid fa-chevron-down ml-2 transition-transform ease-in-out ${showAllRooms ? "rotate-180" : "rotate-0"}`}
-        ></i>
-      </button>
-    </SectionHDP>
+              {/* TITLE + INFOS */}
+              <RoomHeader
+                room={room}
+                className="flex flex-1 justify-center px-2"
+              />
+
+              {/*  BOUTON */}
+              <NavLink
+                to={`/rooms#${room.id}`}
+                className="rounded-md bg-gray-800 p-2 text-center text-sm font-semibold text-white hover:bg-gray-900"
+              >
+                {t("common.moreInfo")}
+              </NavLink>
+            </article>
+          ))}
+        </section>
+      </CollapsedSection>
+    </section>
   );
 };
 
